4bc3413c623c379efd2823d1a22a37f63d26019b
Scott Scoble <sscoble@codesmith.ws>
2013-09-18 11:24:22 -0700

    increment version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='CNSdi',
-    version='0.1.0',
+    version='0.1.1',
     author='Scott Scoble',
     author_email='sscoble@codesmith.ws',
     packages=['di','di.test'],
