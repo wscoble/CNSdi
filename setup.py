376c4d1249ec7211d7519e636152ce43dc85d38b
Scott Scoble <sscoble@codesmith.ws>
2013-09-21 10:32:50 -0700

    increment version, update url

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 setup(
     name='CNSdi',
-    version='0.1.3',
+    version='0.1.4',
     author='Scott Scoble',
     author_email='sscoble@codesmith.ws',
     packages=['di','di.test'],
     license='LICENSE.txt',
-    url='https://github.com/wscoble/CNSdi',
+    url='http://wscoble.github.io/CNSdi',
     description='Simple Dependency Injection for python',
     long_description=open('README.txt').read(),
     install_requires=[]
